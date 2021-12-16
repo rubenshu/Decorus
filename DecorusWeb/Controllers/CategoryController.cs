e82790efab9cd3cc51d39e567ec25681f2256678
rubenshu <ruben.sikkens@student.hu.nl>
2021-12-16 16:28:55 +0100

    Added category items from db to CategoryController

---
+++ DecorusWeb/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using DecorusWeb.Data;
+using DecorusWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DecorusWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+    }
+}
