2604a5236f41e4d00d376e37e9b26c241580688e
rubenshu <ruben.sikkens@student.hu.nl>
2021-12-16 15:44:41 +0100

    Category model, connectionString and Data folder

---
+++ DecorusWeb/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DecorusWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DIsplayOrder { get; set; }
+        public DateTime CreatedDayTime { get; set; } = DateTime.Now;
+    }
+}
