d9b0b61f0d65c036980238ae0fb5ab65a122d398
rubenshu <ruben.sikkens@student.hu.nl>
2021-12-16 15:50:20 +0100

    DbContext

---
+++ DecorusWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using DecorusWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DecorusWeb.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
