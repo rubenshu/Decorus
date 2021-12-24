2d3722244b4a856973ff5426dea8b24569c121be
rubenshu <ruben.sikkens@student.hu.nl>
2021-12-24 10:41:07 +0100

    Foregin key relations Product model

---
+++ Decorus.Models/Product.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorus.Models
+{
+    public class Product
+    {
+
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public string ISBN { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        [Range(1, 10000)]
+        public double Price { get; set; }
+
+        [Required]
+        [Range(1, 10000)]
+        public double Price50 { get; set; }
+
+        [Required]
+        [Range(1, 10000)]
+        public double Price100 { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        // Will automatically make CategoryId a foreign key
+        [Required]
+        public int CategoryId { get; set; }
+        // Can explicitly use ForeignKey tag, but not needed. Only if the name is different, it will not automatically map.
+        // When Id is appended 
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+
+        // Will automatically make CoverTypeId a foreign key
+        [Required]
+        public int CoverTypeId { get; set; }
+        [ForeignKey("CoverType")]
+        public CoverType CoverType { get; set; }
+
+    }
+}
