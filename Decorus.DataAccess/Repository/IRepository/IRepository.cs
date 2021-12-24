89399deede4fae7f016c74542be9bd15d6f7c51c
rubenshu <ruben.sikkens@student.hu.nl>
2021-12-24 17:57:20 +0100

    Small refactor

--- Decorus.DataAccess/Repository/IRepository/IRepository.cs
+++ Decorus.DataAccess/Repository/IRepository/IRepository.cs
@@ -10,7 +10,7 @@ namespace Decorus.DataAccess.Repository.IRepository
     public interface IRepository<T> where T : class
     {
         //T - Category
-        T GetFirstOrDefault(Expression<Func<T, bool>> filter, string? includePropertries = null);
+        T GetFirstOrDefault(Expression<Func<T, bool>> filter, string? includeProperties = null);
         IEnumerable<T> GetAll(string? includeProperties = null);
         void Add(T entity);
         void Remove(T entity);
