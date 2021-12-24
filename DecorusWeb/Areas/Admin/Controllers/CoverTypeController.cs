4ee70811c0863b4e766e05e80f77479aeafd00b7
rubenshu <ruben.sikkens@student.hu.nl>
2021-12-24 10:18:44 +0100

    Formatting

--- DecorusWeb/Areas/Admin/Controllers/CoverTypeController.cs
+++ DecorusWeb/Areas/Admin/Controllers/CoverTypeController.cs
@@ -5,116 +5,115 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DecorusWeb.Controllers;
 [Area("Admin")]
-    public class CoverTypeController : Controller
+public class CoverTypeController : Controller
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CoverTypeController(IUnitOfWork unitOfWork)
     {
-        private readonly IUnitOfWork _unitOfWork;
+        _unitOfWork = unitOfWork;
+    }
 
-        public CoverTypeController(IUnitOfWork unitOfWork)
+    public IActionResult Index()
+    {
+        IEnumerable<CoverType> objCoverTypeList = _unitOfWork.CoverType.GetAll();
+        return View(objCoverTypeList);
+    }
+
+    //GET
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    //POST
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(CoverType obj)
+    {
+        if (obj.Name == obj.Id.ToString())
         {
-            _unitOfWork = unitOfWork;
+            ModelState.AddModelError("Name", "DisplayOrder !== Name");
         }
-
-        public IActionResult Index()
+        if (ModelState.IsValid)
         {
-            IEnumerable<CoverType> objCoverTypeList = _unitOfWork.CoverType.GetAll();
-            return View(objCoverTypeList);
-        }
-
-        //GET
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        //POST
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Create(CoverType obj)
-        {
-            if (obj.Name == obj.Id.ToString())
-            {
-                ModelState.AddModelError("Name", "DisplayOrder !== Name");
-            }
-            if (ModelState.IsValid)
-            {
-                _unitOfWork.CoverType.Add(obj);
-                _unitOfWork.Save();
-                TempData["success"] = "Cover created succesfully";
-                return RedirectToAction("Index", "CoverType");
-            }
-            return View(obj);
-        }
-
-        //GET
-        public IActionResult Edit(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-            //var CoverFromDb = _db.Categories.Find(id);
-            var CoverFromDb = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
-            //var CoverFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
-
-            if (CoverFromDb == null)
-            {
-                return NotFound();
-            }
-
-            return View(CoverFromDb);
-        }
-
-        //POST
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Edit(CoverType obj)
-        {
-            if (obj.Name == obj.Id.ToString())
-            {
-                ModelState.AddModelError("Name", "DisplayOrder !== Name");
-            }
-            if (ModelState.IsValid)
-            {
-                _unitOfWork.CoverType.Update(obj);
-                _unitOfWork.Save();
-                TempData["success"] = "Cover edit succesfully";
-                return RedirectToAction("Index", "CoverType");
-            }
-            return View(obj);
-        }
-
-        //GET
-        public IActionResult Delete(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-            var CoverFromDb = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
-
-            if (CoverFromDb == null)
-            {
-                return NotFound();
-            }
-
-            return View(CoverFromDb);
-        }
-
-        //POST
-        [HttpPost,ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public IActionResult DeletePOST(int? id)
-        {
-            var obj = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
-            if (obj == null)
-            {
-                return NotFound();
-            }
-
-            _unitOfWork.CoverType.Remove(obj);
+            _unitOfWork.CoverType.Add(obj);
             _unitOfWork.Save();
-            TempData["success"] = "Cover deleted succesfully";
-            return RedirectToAction("Index");
+            TempData["success"] = "Cover created succesfully";
+            return RedirectToAction("Index", "CoverType");
         }
+        return View(obj);
+    }
+
+    //GET
+    public IActionResult Edit(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+        //var CoverFromDb = _db.Categories.Find(id);
+        var CoverFromDb = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
+        //var CoverFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+
+        if (CoverFromDb == null)
+        {
+            return NotFound();
+        }
+
+        return View(CoverFromDb);
+    }
+
+    //POST
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(CoverType obj)
+    {
+        if (obj.Name == obj.Id.ToString())
+        {
+            ModelState.AddModelError("Name", "DisplayOrder !== Name");
+        }
+        if (ModelState.IsValid)
+        {
+            _unitOfWork.CoverType.Update(obj);
+            _unitOfWork.Save();
+            TempData["success"] = "Cover edit succesfully";
+            return RedirectToAction("Index", "CoverType");
+        }
+        return View(obj);
+    }
+
+    //GET
+    public IActionResult Delete(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+        var CoverFromDb = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
+
+        if (CoverFromDb == null)
+        {
+            return NotFound();
+        }
+
+        return View(CoverFromDb);
+    }
+
+    //POST
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public IActionResult DeletePOST(int? id)
+    {
+        var obj = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
+        if (obj == null)
+        {
+            return NotFound();
+        }
+
+        _unitOfWork.CoverType.Remove(obj);
+        _unitOfWork.Save();
+        TempData["success"] = "Cover deleted succesfully";
+        return RedirectToAction("Index");
     }
 }
